55790d3712cefe4700ced4aac83f9adf259b8446
Jarrod Swift <jarrod.swift@aligent.com.au>
2020-12-24 09:54:49 +1030

    Use shadow useradd because alpine won't support high UIDs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,19 @@
 
 APP_ROOT="/app"
 
+DOCKER_UID=`stat -c "%u" $APP_ROOT`
+DOCKER_GID=`stat -c "%g" $APP_ROOT`
+
+INCUMBENT_USER=`getent passwd $DOCKER_UID | cut -d: -f1`
+INCUMBENT_GROUP=`getent group $DOCKER_GID | cut -d: -f1`
+
+#echo "Docker: uid = $DOCKER_UID, gid = $DOCKER_GID"
+#echo "Incumbent: user = $INCUMBENT_USER, group = $INCUMBENT_GROUP"
+
+userdel node
+groupadd -g ${DOCKER_GID} node
+useradd -g ${DOCKER_GID} --home-dir /home/node -s /bin/bash -u ${DOCKER_UID} node
+
 chown -R node:node /home/node/.config
 chown -R node:node /home/node/.serverless
 
